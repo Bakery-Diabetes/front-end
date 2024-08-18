cc460484ccdb1f9eaf8c14f2af4bea4898682cf3
hamzasenh <hamza.senhaji.11647@stu.he-ferrer.eu>
2024-08-18 22:21:08 +0200

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Seo } from "@/ui/components/seo";
 export default function Home() {
   return (
     <>
-      <Seo title="Bakery & Diabetes" description="Description..." />
+      <Seo title="Bakery & Diabetes" description="Description.." />
       <h1>Hello world</h1>
     </>
   );
